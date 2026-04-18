480651608f82a010f882fdc925aac57371b452b8
Ahmed Mekheimer <ahmed.khaled.mekheimer@gmail.com>
2026-04-18 19:22:32 +0200

    products model

---
+++ models/productModel.js
@@ -0,0 +1,65 @@
+const mongoose = require('mongoose')
+
+const productSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: [true, 'Product Title Required'],
+            unique: [true, 'Product Title must be unique'],
+            minLength: [3, 'Product Title is too short'],
+            maxLength: [100, 'Product Title is too long']
+        },
+        slug: {
+            type: String,
+            required: true,
+            lowercase: true
+        },
+        category: {
+            type: mongoose.Schema.ObjectId,
+            ref: 'Category',
+            required: [true, 'Category Required'],
+        },
+        subCategories: [{
+            type: mongoose.Schema.ObjectId,
+            ref: 'SubCategory',
+        }],
+        brand: {
+            type: mongoose.Schema.ObjectId,
+            ref: 'Brand',
+        },
+        colors: [String],
+        description: {
+            type: String,
+            required: [true, 'Product Description Required'],
+            minLength: [20, 'Product Description is too short'],
+            maxLength: [2000, 'Product Description is too long']
+        },
+        price: {
+            type: Number,
+            required: [true, 'Product Description Required'],
+        },
+        quantity: {
+            type: Number,
+            required: [true, 'Product Quantity Required'],
+        },
+        soldCounter: {
+            type: Number,
+            default: 0
+        },
+        coverImage: {
+            type: String,
+            required: [true, 'Product cover image is Required'],
+        },
+        images: [String],
+        averageRating: {
+            type: Number,
+            min: [1, 'Rating cannot be smaller than 1'],
+            max: [5, 'Rating cannot be greater tahn 5']
+        },
+        ratingsCounter: {
+            type: Number,
+            default: 0
+        }
+    },
+    { timestamps: true }
+)
